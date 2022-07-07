beaadc810ef73570a97f70625eab7d78391902fa
Brandon Neves <dev.brandon.neves>
2022-07-07 03:02:18 -0300

    Implementado função de buscar ultimos pedidos realizados na api. Implementado função de repetir um pedido já feito, listado na seção de ultimos pedidos

--- script.js
+++ script.js
@@ -3,7 +3,58 @@ let escolherModelo
 let escolherGola
 let escolherTecido
 let urlImagem
+let blusas
 let verificaUrl = /^[a-zA-Z0-9-_]+[:./\\]+([a-zA-Z0-9 -_./:=&"'?%+@#$!])+$/
+
+buscarBlusas()
+function buscarBlusas() {
+  let promise = axios.get(
+    'https://mock-api.driven.com.br/api/v4/shirts-api/shirts'
+  )
+  promise.then(renderizarUltimosPedidos)
+}
+
+function renderizarUltimosPedidos(resposta) {
+  blusas = resposta.data
+  document.querySelector('.ultimos-pedidos').innerHTML = ''
+  for (let i = 0; i <= 10; i++) {
+    document.querySelector('.ultimos-pedidos').innerHTML += `
+    <div class="pedido" onclick="repetirPedido(this)">
+    <div class="fundo-pedido">
+    <p class="modelo-escolhido">${blusas[i].model}</p>
+    <p class="gola-escolhida">${blusas[i].neck}</p>
+    <p class="tecido-escolhido">${blusas[i].material}</p>
+      <img src="${blusas[i].image}" alt="" />
+      <h3><strong>Criador:</strong> <span>${blusas[i].owner}</span></h3>`
+  }
+}
+
+function repetirPedido(pedidoEscolhido) {
+  let pegarModelo = pedidoEscolhido.querySelector('.modelo-escolhido').innerHTML
+  let pegarGola = pedidoEscolhido.querySelector('.gola-escolhida').innerHTML
+  let pegarMaterial =
+    pedidoEscolhido.querySelector('.tecido-escolhido').innerHTML
+  let pegarImagem = pedidoEscolhido.querySelector('img').src
+  let pegarCriador = pedidoEscolhido.querySelector('span').innerHTML
+  let confirmarPedido = confirm(
+    'Você tem certeza que gostaira de pedir esse produto novamente?'
+  )
+  if (confirmarPedido) {
+    let promise = axios.post(
+      'https://mock-api.driven.com.br/api/v4/shirts-api/shirts',
+      {
+        model: pegarModelo,
+        neck: pegarGola,
+        material: pegarMaterial,
+        image: pegarImagem,
+        owner: pegarCriador,
+        author: pegarCriador
+      }
+    )
+    promise.then(encomendaCriada)
+    promise.catch(erroNaEncomenda)
+  }
+}
 
 function modeloEscolhido(modelo) {
   let produtoClicado = document.querySelector('.escolher-modelo .selecionado')
@@ -73,6 +124,7 @@ function confirmarPedido() {
 
 function encomendaCriada() {
   alert('Sua encomenda foi realizada com sucesso')
+  buscarBlusas()
 }
 
 function erroNaEncomenda() {
